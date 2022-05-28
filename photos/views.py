30b5508d802fcb2fb271f77ec259a9d0f6977fea
limu2030 <limu2030@gmail.com>
2022-05-28 17:03:20 +0300

    st: add photos and styling

--- photos/views.py
+++ photos/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 def gallery(request):
     return render(request, 'photos/gallery.html')
 
-def viewPhoto(request, pk):
+def viewPhoto(request):
     return render(request, 'photos/photo.html')
 
 def addPhoto(request):
