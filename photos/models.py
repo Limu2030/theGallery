6ce00ea7d471399f7dfc54a01ab9cf8cb2be4692
limu2030 <limu2030@gmail.com>
2022-05-29 12:05:36 +0300

    ft: create category and photo models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,19 @@
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Photo(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    image = models.ImageField(null=False, blank=False)
+    description = models.TextField(max_length=500, null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.name        
