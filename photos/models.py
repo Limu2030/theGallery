6370c3131714b58ca78368efea622b0accfc9431
limu2030 <limu2030@gmail.com>
2022-05-30 00:59:27 +0300

    ft: create a form to upload image

--- photos/models.py
+++ photos/models.py
@@ -1,5 +1,6 @@
-from unicodedata import category
 from django.db import models
+
+
 
 # Create your models here.
 
@@ -10,10 +11,11 @@ class Category(models.Model):
         return self.name
 
 class Photo(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    id = models.AutoField(primary_key=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     image = models.ImageField(null=False, blank=False)
     description = models.TextField(max_length=500, null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
-        return self.name        
+        return self.description       
--- photos/views.py
+++ photos/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
+from .models import Category, Photo
 
 # Create your views here.
 
 def gallery(request):
-    return render(request, 'pics/gallery.html')
+    categories = Category.objects.all()
+    photos = Photo.objects.all()
 
-def viewPhoto(request):
-    return render(request, 'pics/photo.html')
+    context = {'categories': categories, 'photos': photos}
+    return render(request, 'pics/gallery.html', context)
+
+def viewPhoto(request, pk):
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'pics/photo.html', {'photo': photo})
 
 def addPhoto(request):
     return render(request, 'pics/add.html')
