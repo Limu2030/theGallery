ba3c617d0f1f4799dc830d44653718b69ce900e8
limu2030 <limu2030@gmail.com>
2022-05-30 03:05:24 +0300

    ft: function to create new category

--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Photo
 
 # Create your views here.
@@ -15,6 +15,31 @@ def viewPhoto(request, pk):
     return render(request, 'pics/photo.html', {'photo': photo})
 
 def addPhoto(request):
-    return render(request, 'pics/add.html')
+    categories = Category.objects.all()
+
+    if request.method == 'POST':
+        data = request.POST
+        images = request.FILES.getlist('images')
+
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(
+                 
+                name=data['category_new'])
+        else:
+            category = None
+
+        for image in images:
+            photo = Photo.objects.create(
+                category=category,
+                description=data['description'],
+                image=image,
+            )
+
+        return redirect('gallery')
+
+    context = {'categories': categories} 
+    return render(request, 'pics/add.html', context)
 
 
